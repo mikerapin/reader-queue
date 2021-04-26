83feae3c8efb5df33e657e061261b2db1e3767f0
Mike <mikerapin+windowsgit@gmail.com>
2021-04-26 01:01:31 -0400

    Added a README, Start Reading button, fixed bugs
    - Converted all of the functions to arrow functions
    - Added a basic Readme
    - Fixed an issue with removing items from the queue not filtering
    properly
    - Added Start Reading button to start you at the top of your queue
    - Added some styling to the new button and some button logic
    - Rearranged some templating logic to make some of the creation
    functions cleaner
    - Tweaked id generator to be simpler

---
+++ README.md
@@ -0,0 +1,8 @@
+# comiXology Reader Queue
+A simple extension that allows you to queue items for reading.
+
+When you reach the end of a comic, you'll be prompted to read the next item in your queue. 
+
+Manage your queue in the My Books section of the site.
+
+Author: Mike Rapin - [@mikerapin](https://www.twitter.com/mikerapin)
--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 /**
- * Hey this thing is kind of a mess, but I think it works pretty okay!
+ * Hey this thing is kind of a mess, but I think it works pretty well!
  * 
- * @mikerapin
+ * Author: Mike Rapin - @mikerapin
  */
 
-function queueIcon() {
+
+
+const queueIcon = () => {
     return `
 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
@@ -12,12 +14,28 @@ function queueIcon() {
 `;
 }
 
-function _id() {
-    return '_' + Math.random().toString(36).substr(2, 9);
+const clearIcon = () => {
+    return `
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg>
+`;
 }
 
-function addToQueue(e) {
-    let data = e.target.dataset;
+const readIcon = () => {
+    return ` 
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+</svg>
+`;
+}
+
+const _id = () => {
+    return '_' + Math.random().toString(36);
+}
+
+const addToQueue = (e) => {
+    const data = e.target.dataset;
     chrome.storage.sync.get(['readqueue'], (result) => {
         let readQueue;
         if (!result.length && typeof result.readqueue === 'undefined') {
@@ -25,60 +43,74 @@ function addToQueue(e) {
         } else {
             readQueue = result.readqueue;
         }
+        if (readQueue.length) {
+            const index = readQueue.findIndex((qItem) => {
+                return qItem.id === data.id && qItem.sid === data.sid 
+            });
+            if (index > -1) {
+                readQueue.splice(index, 1);
+            }
+        }
         readQueue.push({
             'uid': _id(),
-            'title': data.title,
+            'title': data.title.trim(),
             'sid': data.sid,
             'id': data.id,
         });
         chrome.storage.sync.set({ 'readqueue': readQueue }, () => {
-            console.log('adding ' + data.title + ' to the queue');
-            console.log(readQueue);
             createManager();
         })
     });
 }
 
-function removeFromQueue(e) {
-    let uid = e.target.dataset.uid;
-    console.log(e.target.dataset)
+const removeFromQueue = (e) => {
+    const uid = e.currentTarget.dataset.uid;
     chrome.storage.sync.get(['readqueue'], (result) => {
         let readQueue;
-        if (!result.length && typeof result.readqueue === 'undefined' && uid !== null) {
+        if (!result.length && typeof result.readqueue === 'undefined' || uid === null) {
             return;
         } else {
             readQueue = result.readqueue;
         }
 
-        let index = readQueue.findIndex(item => item.uid === uid);
-        readQueue.splice(index, 1);
+        const index = readQueue.findIndex(item => item.uid === uid);
 
-        chrome.storage.sync.set({ 'readqueue': readQueue }, () => {
-            console.log(readQueue);
-            createManager();
-        });
+        if (index > -1) {
+            readQueue.splice(index, 1);
+            chrome.storage.sync.set({ 'readqueue': readQueue }, () => {
+                createManager();
+            });
+        }
     });
 }
 
-function clearQueue() {
+const clearQueue = () => {
     if (confirm("are you sure you want to clear the queue???")) {
         chrome.storage.sync.set({ 'readqueue': [] }, () => {
-            console.log("Cleared the queue", []);
             createManager();
         });
     }
 }
 
-function buildReaderButtons() {
-    console.log('adding queue buttons')
-    let bookContainer = document.querySelectorAll('.lv2-book-item, .lv2-book-micro-item');
+const startReading = () => {
+    chrome.storage.sync.get(['readqueue'], (result) => {
+        let readQueue;
+        if (!result.length && typeof result.readqueue === 'undefined') {
+            return;
+        }
+        const firstItem = result.readqueue[0];
+        window.location.href = `/comic-reader/${firstItem.sid}/${firstItem.id}`;
+    });
+}
+
+const buildReaderButtons = () => {
+    const bookContainer = document.querySelectorAll('.lv2-book-item, .lv2-book-micro-item');
     bookContainer.forEach(container => {
         const readerButton = container.querySelector('.lv2-read-button');
         const gridReadUrlLink = container.querySelector('.lv2-item-link');
         const microReadUrlLink = container.querySelector('.lv2-micro-item-title-container');
 
         if ((readerButton === null || gridReadUrlLink === null) && (readerButton === null || microReadUrlLink === null)) {
-            console.log('no button!!', gridReadUrlLink, microReadUrlLink)
             return;
         }
 
@@ -102,37 +134,11 @@ function buildReaderButtons() {
     });
 }
 
-function createManager() {
-    let queueManagerContainer = document.querySelector('.queue-manager-container');
-    if (document.querySelector('.queue-manager')) {
-        document.querySelector('.queue-manager').remove();
-    }
-    let queueManager = document.createElement('div');
-    let clearQueueButton = document.createElement('button');
-
-    queueManager.classList.add('queue-manager');
-    queueManager.textContent = '~Loading~';
-    clearQueueButton.classList.add('queue-clear');
-    clearQueueButton.innerHTML = `
-<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-</svg>
-    `;
-    clearQueueButton.addEventListener('click', clearQueue)
-
-    let content = '';
-    chrome.storage.sync.get(['readqueue'], (result) => {
-        let readQueue;
-        if (!result.length && typeof result.readqueue === 'undefined') {
-            readQueue = [];
-        } else {
-            readQueue = result.readqueue;
-        }
-        readQueue.forEach(item => {
-            content += `
+const createQueueItem = (item) => {
+    return `
 <div class="queue-item-container">
     <div class="queue-item">
-        <a href="/comic-reader/${item.sid}/${item.id}">${item.title}</a>
+        ${item.title}
     </div>
     <div class="queue-item-remove">
         <button class="remove-queue-item" data-uid="${item.uid}">
@@ -143,11 +149,67 @@ function createManager() {
     </div>
 </div>
 `;
+}
+
+const emptyQueueItem = () => {
+    return `<div class="queue-item-container"><div class="queue-item">Empty Queue</div></div>`;
+}
+
+const createQueueButtons = (showEmptyQueueButton) => {
+    const buttonGroup = document.createElement('div');
+    const clearQueueButton = document.createElement('button');
+    const startReadingButton = document.createElement('button');
+    const buttons = [];
+    
+    buttonGroup.classList.add('queue-button-group');
+
+    startReadingButton.classList.add('queue-start-reading');
+    startReadingButton.innerHTML = '<span>Start Reading</span>';
+    startReadingButton.addEventListener('click', startReading);
+    buttons.push(startReadingButton);
+
+    if (showEmptyQueueButton) {
+        clearQueueButton.classList.add('queue-clear', 'queue-button');
+        clearQueueButton.innerHTML = clearIcon();
+        clearQueueButton.addEventListener('click', clearQueue)
+        buttons.push(clearQueueButton);
+    }
+
+    buttonGroup.append(...buttons);
+
+    return buttonGroup;
+}
+
+const createManager = () => {
+    const queueManagerContainer = document.querySelector('.queue-manager-container');
+    if (document.querySelector('.queue-manager')) {
+        document.querySelector('.queue-manager').remove();
+    }
+    const queueManager = document.createElement('div');
+
+    queueManager.classList.add('queue-manager');
+    queueManager.textContent = '~Loading~';
+
+    let content = emptyQueueItem();
+    chrome.storage.sync.get(['readqueue'], (result) => {
+        let readQueue;
+        if (!result.length && typeof result.readqueue === 'undefined') {
+            readQueue = [];
+        } else {
+            readQueue = result.readqueue;
+        }
+
+        content = result.readqueue.length ? '' : content;
+        const showEmptyQueueButton = readQueue.length > 0;
+        const buttonsGroup = createQueueButtons(showEmptyQueueButton);
+
+        readQueue.forEach(item => {
+            content += createQueueItem(item)
         });
-        content = `<div class="queue-items">${content}</div>`;
-        queueManager.innerHTML = content;
-        queueManager.appendChild(clearQueueButton);
-        let removeFromQueueButtons = queueManager.querySelectorAll('.remove-queue-item');
+        queueManager.innerHTML = `<div class="queue-items">${content}</div>`;
+        queueManager.appendChild(buttonsGroup);
+
+        const removeFromQueueButtons = queueManager.querySelectorAll('.remove-queue-item');
         removeFromQueueButtons.forEach(button => {
             button.addEventListener('click', removeFromQueue);
         });
@@ -155,7 +217,7 @@ function createManager() {
     queueManagerContainer.appendChild(queueManager);
 }
 
-function triggerManager(e) {
+const triggerManager = (e) => {
     let manager = document.querySelector('.queue-manager');
     if (manager === null) {
         createManager();
@@ -164,9 +226,9 @@ function triggerManager(e) {
     }
 }
 
-function buildQueueManager() {
-    let queueManagerContainer = document.createElement('div');
-    let queueManagerToggle = document.createElement('button');
+const buildQueueManager = () => {
+    const queueManagerContainer = document.createElement('div');
+    const queueManagerToggle = document.createElement('button');
 
     queueManagerContainer.classList.add('queue-manager-container');
     queueManagerToggle.innerHTML = queueIcon();
@@ -177,11 +239,10 @@ function buildQueueManager() {
     document.body.appendChild(queueManagerContainer);
 }
 
-function createQueue() {
-    console.log('creating the reading queue');
+const createQueue = () => {
     const tabContent = document.querySelector('.lv2-tab-content');
-    let bookContainer = document.querySelectorAll('.lv2-book-item, .lv2-book-micro-item');
-    let readButtons = document.querySelectorAll('.lv2-read-button');
+    const bookContainer = document.querySelectorAll('.lv2-book-item, .lv2-book-micro-item');
+    const readButtons = document.querySelectorAll('.lv2-read-button');
     if (!bookContainer.length || !readButtons.length) {
         window.requestAnimationFrame(createQueue);
     } else {
@@ -190,9 +251,9 @@ function createQueue() {
     }
 }
 
-function initialize() {
+const initialize = () => {
     const tabContent = document.querySelector('.lv2-tab-content');
-    const timer = setInterval(function() {
+    const timer = setInterval(() => {
         if (tabContent && !tabContent.classList.contains('lv2-loading')) {
             tabContent.id = 'lv2-tab-content';
             createQueue();
@@ -201,7 +262,6 @@ function initialize() {
             const contentById = document.getElementById('lv2-tab-content')
             const observer = new MutationObserver(buildReaderButtons);
             if (contentById) {
-                console.log('observin')
                 observer.observe(contentById, {
                     childList: true
                 });
